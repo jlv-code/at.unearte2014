690075fe66a4a088fc7b321090da3674dfbeaa66
jlv <jvieitez@mippci.gob.ve>
2014-02-12 16:42:31 -0430

    Nuevos cambios

--- index.php
+++ index.php
@@ -1,57 +1,11 @@
 	<?php get_header() ?>
 	<div class="inner-content">
 		<section class="the-content">
-			<header class="the-title"><h1 class="title">La Coordinación</h1></header>	
+			<header class="the-title"><h1 class="title">Sin funcionamiento</h1></header>	
 			<article class="content">
 				<p>
-					Esta entidad fue creada en mayo de 2013 y está adscrita directamente al Rectorado. 
-					La integra un equipo de miembros de esta universidad, actualmente conformado por 
-					profesores y estudiantes.
+					Actualmente ésta página no se encuentra desarrollada, por favor vaya a Aperiencia > Personalizar en el menú del Administrador de Contenido para cambiarla.
 				</p>
-				<p>
-					La Coordinación de Investigación de Artes Tradicionales 
-					está trabajando en base a un proyecto general bajo el título Vinculación solidaria 
-					entre la Universidad de las Artes y comunidades con maestros honorarios tradicionales. 
-					Fortalecimiento de la independencia identitaria y científica. En este marco se propone:
-				</p>
-				<ol>
-					<li>
-					dirigir la actividad de la universidad como ente generador de conocimientos 
-					específicos para atenderlas necesidades preponderantes de las comunidades: populares, 
-					académica y del país.
-					</li>
-    				<li>
-    				contribuir al empoderamiento de las artes tradicionales en los diferentes tipos de 
-    				comunidades.
-    				</li>
-    				<li>
-    				generar productos con impacto social entre universidad, maestros honorarios y sus 
-    				comunidades.
-    				</li>
-    				<li>
-    				atender las exigencias formativas de la universidad en artes tradicionales en contextos 
-    				reales.
-    				</li>
-				</ol>
-				<p>
-					Los proyectos concretos en un caso ideal son realizados por equipos multidisciplinarios  
-					en distintos lugares del país y se enmarcan en la investigación-reflexión-acción 
-					participativa, la semiótica y el análisis del discurso. Parte fundamental de la metodología 
-					es la permanente cooperación y coproducción entre estudiantes, profesores universitarios, 
-					artistas de las comunidades y organismos locales como Consejos comunales, Asociaciones civiles, 
-					Cooperativas, entre otros, para formar cuadros individuales o colectivos con facultades y un 
-					desarrollado sentido de investigación aplicada. Ésta se dirige hacia el beneficio concreto y 
-					necesario de una comunidad, ya sea académica, comunitaria o nacional y fortalecerá nuestra 
-					independencia cultural y científica.
-				</p>
-				<p>
-					Dentro de Unearte la Coordinación actúa como centro de orientación para la integración o 
-					creación de proyectos y es además centro operativo de vinculación con las comunidades y las 
-					actividades propias de la universidad, que puedan  coincidir con temas de diferentes unidades
-					curriculares de los PNF, la difusión de producciones a nivel electrónico y otras.
-				</p>
-				<p><strong>Dra. Katrin Lengwinat<br/>Coordinadora</strong></p>
-				<p><img src="<?php print get_template_directory_uri() ?>/images/mascara.png"></p>
 			</article>
 		</section>
 		<?php get_sidebar() ?>
