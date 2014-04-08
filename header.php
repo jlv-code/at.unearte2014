fb412c3d5cdcfc0e1d9f4e8847afe245ed217ea5
jlv <jvieitez@mippci.gob.ve>
2014-04-08 16:42:30 -0430

    Agregado Analytics

--- header.php
+++ header.php
@@ -16,6 +16,17 @@
 <meta name="description" content="Sitio web relacionado con las investigaciones hechas por la Coordinación de Investigación de Artes Tradicionales de la Universidad Nacional Experimental de las Artes (UNEARTE) en Venezuela" />
 <meta name="keywords" content="artes, artes tradicionales, investigación, investigaciones, unearte, universidad de artes, coordinación de investigación" />
 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-31218036-2', 'unearte.edu.ve');
+  ga('send', 'pageview');
+
+</script>
+
 <?php wp_head() ?>
 
 </head>
